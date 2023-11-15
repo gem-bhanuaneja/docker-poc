507bb6d549fdd3a29df642e2a5b212bd265f043b
gem-bhanuaneja <106229832+gem-bhanuaneja@users.noreply.github.com>
2023-11-15 10:11:39 +0530

    docker poc implemented in crud operation

--- Docker-springboot/Docker-springboot/src/test/java/com/example/Dockerspringboot/DockerSpringbootRedisPostgresApplicationTests.java
+++ Docker-springboot/Docker-springboot/src/test/java/com/example/Dockerspringboot/DockerSpringbootRedisPostgresApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.Dockerspringboot;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DockerSpringbootRedisPostgresApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.example.Dockerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringbootRedisPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
